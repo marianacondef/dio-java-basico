465f6d913677bc55c9a01a0f2d29ac3b959f9512
marianacondef <marianacondefidelis@gmail.com>
2023-10-14 19:28:28 -0300

    operadores 2

--- operadores/src/App.java
+++ operadores/src/App.java
@@ -1,8 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String primeiroNome = "Mariana";
-        String segundoNome = "Conde";
-        System.out.println((primeiroNome + " " + segundoNome));
+        String primeiroNum = "1";
+        String segundoNum = "5";
+        String num = primeiroNum + segundoNum;
+        System.out.println(num);
 
 
     }
