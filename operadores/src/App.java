c005565a01551fcd5d1d582b7da71fcf57c325ec
marianacondef <marianacondefidelis@gmail.com>
2023-10-14 19:19:27 -0300

    aula de operadores

---
+++ operadores/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String primeiroNome = "Mariana";
+        String segundoNome = "Conde";
+        System.out.println((primeiroNome + " " + segundoNome));
+
+
+    }
+}
