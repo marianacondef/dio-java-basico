20a7a1b348ce14bb5b650612d0e47c19b869b4bc
marianacondef <marianacondefidelis@gmail.com>
2023-10-14 19:44:58 -0300

    aula 4 de operadores

--- operadores/src/operadores.java
+++ operadores/src/operadores.java
@@ -1,9 +1,13 @@
-public class App {
+public class operadores {
     public static void main(String[] args) throws Exception {
         String primeiroNum = "1";
         String segundoNum = "5";
         String num = primeiroNum + segundoNum;
         System.out.println(num);
+
+        boolean bool = true;
+        bool = !bool;
+        System.out.println(bool);
 
 
     }
